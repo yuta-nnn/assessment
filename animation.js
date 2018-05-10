4e4b53a80ef877c3b20ee70f1f562cc8a4c148ca
yuta-nnn <yuuta_17n1100137@nnn.ed.jp>
2018-05-10 11:31:58 +0900

    Create animation.js

---
+++ animation.js
@@ -0,0 +1,17 @@
+(function(){
+    'use strict';
+    var header = document.getElementById('header');
+    header.style.transform = 'rotateX(60deg)';
+    var deg = 0;
+    function rotateHeader() {
+        deg = deg + 6;
+        deg = deg % 360;
+        if ((0 <= deg && deg <90) || (270 <= deg && deg < 360)) {
+            header.className = 'face';
+        } else {
+            header.className = 'back';
+        }
+        header.style.transform = 'rotateX(' + deg + 'deg)';
+    }
+    setInterval(rotateHeader, 20);
+})();
